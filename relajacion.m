5c5aeebcf4cada107bebde3314198ce60bba2982
albertitop17 <152536610+albertitop17@users.noreply.github.com>
2023-12-04 00:01:46 +0100

    Update relajacion.m

--- relajacion.m
+++ relajacion.m
@@ -8,41 +8,29 @@ maxIter = input("Introduce el número máximo de iteraciones:");
 tol = input("Introduce la precision para el test de parada:");
 
 n = length(b);
-x = zeros(n, 1);
+u = zeros(n, 1);
 
-%Calculamos la matriz L
+%Calculamos la matriz Lw
 D = diag(diag(A));
 E = -(tril(A) - D);
 F = -(triu(A) - D);
-L = (((D/w)-E)^(-1))*((((1-w)/w)*D) + F);
+Lw = inv(D-w*E)*((1-w)*D +w*F);
 
-disp('Matriz L:');
-disp(L);
+disp('Matriz Lw:');
+disp(Lw);
 
 iter = 0;
 while iter < maxIter
-    x_viejo = x;
+    u_anterior = u;
     for i = 1:n
-        suma = 0;
-        for j = 1:i-1
-            suma = suma + A(i, j) * x(j);
-        end
-
-        suma2 = 0;
-        for p = i:n
-            suma2 = suma2 + A(i, p) * x_viejo(p);
-        end
-
-        r = b(i) - suma - suma2;
-        d = w * (r / A(i, i));
-        x(i) = x_viejo(i) + d;
+        u(i) = u_anterior(i) + w * ((b(i) - A(i,1:i-1)*u(1:i-1) - A(i,i:n)*u_anterior(i:n)) / A(i, i));
     end
-
-    if norm(x -x_viejo,"inf") < tol
+    
+    if norm(u -u_anterior,"inf") < tol
         disp('El test de parada se alcanza en la iteración:');
         disp(iter);
         disp('El resultado es:');
-        disp(x);
+        disp(u);
         return;
     end
 
@@ -51,7 +39,6 @@ end
 
 disp('Se alcanzó el número máximo de iteraciones sin llegar a la tolerancia.');
 disp('El resultado actual es:');
-disp(x);
-
+disp(u);
 
 
