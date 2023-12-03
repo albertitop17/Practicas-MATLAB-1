fce8b8d0723dab2e6d3f292cfddf9863fe54ff00
albertitop17 <152536610+albertitop17@users.noreply.github.com>
2023-12-03 23:13:20 +0100

    NewUpdate Jacobi.m

--- Jacobi.m
+++ Jacobi.m
@@ -5,54 +5,46 @@ maxIter = input("Introduce el número máximo de iteraciones:");
 tol = input("Introduce la precisión para el test de parada:");
 
 [~,n] = size(A);
+%n = length(A) ??
 
 %Comprobaciones previas
 d = diag(A);
-for i = 1: n
-    if d(i) == 0
+if any(d == 0)
         disp('La matriz A no es valida, debes introducir otra')
         return
-    end
 end
 
 % Hace falta calcular J?
 
 D = diag(diag(A));
-P = D^(-1);
+P = inv(D);
 J = eye(n) - P * A;
+
 disp('La matriz de Jacobi será:')
 disp(J)
 
-autovalores = eig(J); 
-abs_auto = abs(eig(J));
 radio_espectral = max(abs(eig(J)));
 
-if radio_espectral > 1
+if radio_espectral >= 1
     disp('El método no converge. No da la solución del sistema.')
     return
 end
 
-x = zeros(n, 1);
+u = zeros(n, 1);
 
 iter = 0;
 while iter < maxIter
-    x_viejo = x;
+    u_viejo = u;
     for i = 1:n
-        suma = 0;
-        for j = 1:n
-            suma = suma + A(i, j) * x_viejo(j);
-        end
-        r = b(i) - suma;
-        d = r / A(i, i);
-        x(i) = x_viejo(i) + d;
+        u(i) = u_viejo(i) +  (b(i) - A(i,:)*u_viejo)  / A(i, i) 
     end
     
     % Verificar el test de parada
-    if norm(x - x_viejo, "inf") < tol
+    if norm(u - u_viejo, "inf") < tol
         disp('El test de parada se alcanza en la iteración:')
         disp(iter)
         disp('El resultado es:')
-        disp(x)
+        disp(u)
         return
     end
     
@@ -61,4 +53,4 @@ end
 
 disp('Se alcanzó el número máximo de iteraciones sin llegar a la tolerancia.');
 disp('El resultado actual es:')
-disp(x)
+disp(u)
